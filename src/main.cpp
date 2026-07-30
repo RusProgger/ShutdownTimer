87f66c2ccf6f6a92576d37585d369994896a412d
RusProgger <ruslantimka92@gmail.com>
2026-07-30 14:35:28 +0300

    Update menu

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,14 @@ int main() {
     header();
     menu();
 
+    int choice;
 
+    if(choice == 1) {
+        system("shutdown /s /t 0");
+    } else if (choice == 0) {
+        std::cout << "Exit...\n";
+        return 0;
+    }
     
 
     // pause 
