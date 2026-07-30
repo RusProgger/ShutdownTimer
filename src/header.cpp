0b8a90137428a1016cf9c4c5d0f38498f787b107
RusProgger <ruslantimka92@gmail.com>
2026-07-30 14:16:35 +0300

    Update header.cpp

--- src/header.cpp
+++ src/header.cpp
@@ -14,4 +14,7 @@ void header() {
         colorInfo(7);
         std::cout << "-------------------------------------------------\n\n";
         
+        colorInfo(11);
+        std::cout << "A lightweight C++ console application that automatically shuts down the computer after a specified time. \nThe program starts a countdown and executes the system shutdown command when the timer expires. It is fast, simple, and easy to use.\n\n";
+        colorInfo(7);
 }
